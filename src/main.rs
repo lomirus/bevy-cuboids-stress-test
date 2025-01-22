cc5d8efc81f0be7d626bc19cf7bc92e630ad9edb
miruslu <miruslu@tencent.com>
2025-01-22 15:50:44 +0800

    GPU instancing

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::f32::consts::PI;
 
 use bevy::{dev_tools::fps_overlay::FpsOverlayPlugin, prelude::*};
 
+const HALF_SIDE_LENGTH: i32 = 125;
+
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
@@ -11,14 +13,18 @@ fn setup(
 
     commands.spawn((
         Camera3d::default(),
-        Transform::from_xyz(0.0, 0.0, -50.0 / (PI / 8.0).tan()).looking_at(Vec3::ZERO, Vec3::Y),
+        Transform::from_xyz(0.0, 0.0, -(HALF_SIDE_LENGTH as f32) / (PI / 8.0).tan())
+            .looking_at(Vec3::ZERO, Vec3::Y),
     ));
 
-    for x in -50..=50 {
-        for y in -50..=50 {
+    let mesh = meshes.add(Cuboid::from_length(0.5));
+    let material = materials.add(Color::srgb(1.0, 1.0, 1.0));
+
+    for x in -HALF_SIDE_LENGTH..=HALF_SIDE_LENGTH {
+        for y in -HALF_SIDE_LENGTH..=HALF_SIDE_LENGTH {
             commands.spawn((
-                Mesh3d(meshes.add(Cuboid::from_length(0.5))),
-                MeshMaterial3d(materials.add(Color::srgb(1.0, 1.0, 1.0))),
+                Mesh3d(mesh.clone()),
+                MeshMaterial3d(material.clone()),
                 Transform::from_xyz(x as f32, y as f32, 0.0),
             ));
         }
